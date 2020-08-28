98edc85d30b46a59e42fdac79308fb762603d9e7
Umar Arshad <umararshad1995@gmail.com>
2020-08-27 21:37:56 -0400

    header.js

---
+++ empdirectory/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <header>
+                {/* logo image */}
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRhIynvtcO-zSSZ1WWYH3tE6U-fJIe07owN6A&usqp=CAU" alt="Company Logo"></img>
+                {/* title of the company */}
+                <h1> Bubble Club</h1>
+            </header>
+
+        </div>
+    )
+}
+
+export default Header;
